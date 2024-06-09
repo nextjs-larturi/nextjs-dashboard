01883cd083524ac958370db358d9a6fb380078e8
Leandro Arturi <lea.arturi@gmail.com>
2024-06-09 11:07:46 -0300

    Pokemon Single Page

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -16,16 +16,14 @@ export default function CartCounter({ value = 0 }: Props) {
       <div className='flex'>
         <button
           onClick={() => setCounter(counter - 1)}
-          className='flex items-center justify-center p-2 rounded-md bg-gray-900 text-white 
-                 hover:bg-gray-600 transition-all w-[100px] mr-2'
+          className='flex items-center justify-center p-2 rounded-md bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2'
         >
           -1
         </button>
 
         <button
           onClick={() => setCounter(counter + 1)}
-          className='flex items-center justify-center p-2 rounded-md bg-gray-900 text-white 
-                 hover:bg-gray-600 transition-all w-[100px] mr-2'
+          className='flex items-center justify-center p-2 rounded-md bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2'
         >
           +1
         </button>
